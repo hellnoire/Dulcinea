dc6046ce0adffc26f44c458f75acbd3b2e8cda22
Captain-Flinty <34821682+Captain-Flinty@users.noreply.github.com>
2017-12-26 03:29:27 -0700

    Add files via upload

---
+++ modules/games/eightball.py
@@ -0,0 +1,6 @@
+import random
+
+eightball_list = ['hell no', 'it is a risk', 'maybe...', 'sure, why not?', 'hell yeah!']
+
+def eightball():
+    return random.choice(eightball_list)
---
+++ modules/games/snark.py
@@ -0,0 +1,7 @@
+import random
+
+snark_list = ['What the hell did I tell you?', 'Can you not please?', 'Wut.', 'Do you want to be banned?',
+              'I\'m telling Flinty!']
+
+def snark():
+    return random.choice(snark_list)
