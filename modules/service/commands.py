579cfb54656cebf371f7da345bb88aaaedf612a0
Captain-Flinty <34821682+Captain-Flinty@users.noreply.github.com>
2017-12-26 03:31:22 -0700

    Add files via upload

---
+++ modules/service/commands.py
@@ -0,0 +1,7 @@
+summary = '```Commands: \n \n !review = add reviewer role to user \n \n [Critique] = removes review role from user ' \
+          '\n \n ' \
+          '!8ball = magic 8 ball feature! Ask any question! \n \n !Snark = do not, under any circumstances, use this ' \
+          'command```'
+
+def commands():
+    return summary
---
+++ modules/service/help.py
@@ -0,0 +1,6 @@
+summary = 'Dulcinea [BOT] is a Discord bot designed for writing servers, utilizing  multiple quality of life features ' \
+          'and widgets. I am here to help! ' \
+          '\n \n For further commands, enter !Commands'
+
+def help():
+    return summary
