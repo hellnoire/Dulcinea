f9347c4ce18e8a947d73552260964394c7da4e53
Captain-Flinty <34821682+Captain-Flinty@users.noreply.github.com>
2017-12-26 22:58:35 -0700

    Add files via upload

--- dulcinea/client.py
+++ dulcinea/client.py
@@ -1,6 +1,8 @@
 import discord
+from PyDictionary import PyDictionary
 
 client = discord.Client()
+dictionary = PyDictionary()
 
 @client.event
 async def on_ready():
@@ -30,22 +32,48 @@ async def on_message(message):
         await client.send_message(message.channel, master())
     elif message.content.startswith('!Snark'):
         await client.send_message(message.channel, snark())
+    elif message.content.startswith('!define'):
+        word = message.content.split(' ')[1]
+        try:
+            await client.send_message(message.channel, dictionary.meaning(word))
+        except discord.Forbidden:
+            await client.send_message(message.channel, "No bueno.")
+        except discord.errors.HTTPException:
+            await client.send_message(message.channel, "I'm sorry, I'm drawing a blank. Can you try a different word?")
+    elif message.content.startswith('!synonym'):
+        word = message.content.split(' ')[1]
+        try:
+            await client.send_message(message.channel, dictionary.synonym(word))
+        except discord.Forbidden:
+            await client.send_message(message.channel, "No bueno.")
+        except discord.errors.HTTPException:
+            await client.send_message(message.channel, "I'm sorry, I'm drawing a blank. Can you try a different word?")
+    elif message.content.startswith('!antonym'):
+        word = message.content.split(' ')[1]
+        try:
+            await client.send_message(message.channel, dictionary.antonym(word))
+        except discord.Forbidden:
+            await client.send_message(message.channel, "No bueno.")
+        except discord.errors.HTTPException:
+            await client.send_message(message.channel, "I'm sorry, I'm drawing a blank. Can you try a different word?")
 # reviewer automation
-    elif message.content.startswith('!review'):
-        role = discord.utils.get(message.server.roles, name="Reviewer")
+    elif message.content.startswith('!Review'):
+        role = discord.utils.get(message.server.roles, name="Writ-on")
         try:
             await client.add_roles(message.author, role)
+            await client.send_message(message.channel, 'Thank you for the review! You can post content now!')
         except discord.Forbidden:
             await client.send_message(message.channel, "I cannot add this role.")
     elif message.content.startswith('[Critique]'):
-        role = discord.utils.get(message.server.roles, name="Reviewer")
+        role = discord.utils.get(message.server.roles, name="Writ-on")
         try:
             await client.remove_roles(message.author, role)
+            await client.send_message(message.channel, 'Thanks for posting! I hope you get a nice review!')
         except discord.Forbidden:
             await client.send_message(message.channel, "I cannot remove this role.")
     else:
         pass
 try:
-    client.run('Token')
+    client.run('Mzk0NzIxMTM3NjgwMDU2MzIz.DSIciQ.f7G1oR2TRORMrSeshf0ugiebkEE')
 except discord.errors.LoginFailure:
     print("Failed to authenticate into Discord with provided token")
